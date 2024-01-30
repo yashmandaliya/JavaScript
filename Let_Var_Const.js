52c7f6219a24738dc37cdd5552744cfc469a7773
Yash Mandaliya <112624754+yashmandaliya@users.noreply.github.com>
2024-01-30 11:37:02 +0530

    Let_Var_Const.js

---
+++ Let_Var_Const.js
@@ -0,0 +1,23 @@
+// DECLARE USING LET
+let a = 20;
+{
+  let a = 30;
+  console.log(a); //30
+}
+console.log(a); //20
+
+// DECLARE USING VAR
+var b = 20;
+{
+  var b = 40;
+  console.log(b); //40
+}
+console.log(b); //It Gets New Value In B  = 40
+
+// DECLARE USING CONST
+const c = 20;
+{
+  const c = "Yash";
+  console.log(c); //Yash
+}
+console.log(c); //20
