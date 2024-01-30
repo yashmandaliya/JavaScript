b05cd45f9c2ac2b839e5749a4c58595002d69a6d
Yash Mandaliya <112624754+yashmandaliya@users.noreply.github.com>
2024-01-30 11:47:30 +0530

    Object_In_JS.js

---
+++ Object_In_JS.js
@@ -0,0 +1,11 @@
+// Object in JavaScript
+let obj = {
+  name: "Yash",
+  surname: "Mandaliya",
+};
+console.log(obj["name"]); //Yash
+console.log(obj["surname"]); //Mandaliya
+ 
+console.log(typeof obj); // Type Of Varialble - Return  -> Object
+
+
