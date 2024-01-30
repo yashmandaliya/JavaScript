cff2e3f3741f75ba20de571c95cd7e5956417676
Yash Mandaliya <112624754+yashmandaliya@users.noreply.github.com>
2024-01-30 11:29:53 +0530

    HelloWorld.js

---
+++ HelloWorld.js
@@ -0,0 +1,3 @@
+console.log("Hello"); //Print Hello In Console To Check Press F12 Key In Your Keyboard
+document.write("Hello"); // Print InnerHTML Hello
+window.alert("Hello"); //Print Hello In Alert Box
