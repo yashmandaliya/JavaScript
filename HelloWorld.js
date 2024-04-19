bbaef69ca9151d082ccd651db0a71e384fd7ba52
yashmandaliya <info.yashmandaliya@gmail.com>
2024-04-19 16:06:27 +0530

    Add Heading inIndex.html File

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,3 +1,4 @@
 console.log("Hello"); //Print Hello In Console To Check Press F12 Key In Your Keyboard
 document.write("Hello"); // Print InnerHTML Hello
 window.alert("Hello"); //Print Hello In Alert Box
+console.log("yash mandaliya");
